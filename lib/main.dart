3960f3e6c7d7154336c9d4e591a7cf8298931958
Tim Eitel <timeitel@outlook.com>
2020-07-30 17:46:11 +0800

    Add stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,95 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
+      home: HomeCard(),
     ));
 
-class Home extends StatelessWidget {
+class HomeCard extends StatefulWidget {
+  @override
+  _HomeCardState createState() => _HomeCardState();
+}
+
+class _HomeCardState extends State<HomeCard> {
+  int winnings = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('Poker Night'),
+        title: Text('Player Score'),
         centerTitle: true,
-        backgroundColor: Colors.amber[300],
+        backgroundColor: Colors.grey[800],
+        elevation: 0,
       ),
-      body: Center(child: Image.asset('assets/2.jpg')),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('+'),
-        backgroundColor: Colors.amber[300],
+        onPressed: () {
+          setState(() {
+            winnings++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/avatar.jpg'),
+                  radius: 40.0,
+                ),
+              ),
+              Divider(
+                height: 60.0,
+                color: Colors.grey[100],
+              ),
+              Text(
+                'NAME',
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+              ),
+              Text(
+                'Ethan Smith',
+                style: TextStyle(
+                    color: Colors.blue,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'WINNINGS',
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+              ),
+              Text(
+                '$winnings',
+                style: TextStyle(
+                    color: Colors.blue,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 20.0),
+                  Text(
+                    'esmith@hotmail.com',
+                    style: TextStyle(
+                        color: Colors.grey[400],
+                        fontSize: 18.0,
+                        letterSpacing: 1.0),
+                  )
+                ],
+              )
+            ]),
       ),
     );
   }
